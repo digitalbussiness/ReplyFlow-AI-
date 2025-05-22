ce9deae9a37bc921d21a5c8b51b9ad276432456f
digitalbussiness <digitalbussiness331@gmail.com>
2025-05-22 19:57:29 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,9 @@
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Signup from "./pages/Signup";
-import Login from "./pages/Login";
-import Dashboard from "./pages/Dashboard";
-import Home from "./pages/Home"; // ⬅️ importe a nova Home
-import ProtectedRoute from "./components/ProtectedRoute";
+import Landing from "./pages/Landing"; // Certifique-se de importar!
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} /> {/* ⬅️ define como rota raiz */}
+      <Route path="/" element={<Landing />} />
       <Route path="/signup" element={<Signup />} />
       <Route path="/login" element={<Login />} />
       <Route
@@ -20,9 +14,7 @@ function App() {
           </ProtectedRoute>
         }
       />
-      <Route path="*" element={<Home />} /> {/* fallback para Home */}
+      <Route path="*" element={<Login />} />
     </Routes>
   );
 }
-
-export default App;
