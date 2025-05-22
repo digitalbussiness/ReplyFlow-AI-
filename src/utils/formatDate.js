153ebeb2f66c5e29f8ebe0f14e61f3aa099ca4d3
digitalbussiness <digitalbussiness331@gmail.com>
2025-05-22 02:55:01 -0300

    Create formatDate.js

---
+++ src/utils/formatDate.js
@@ -0,0 +1,8 @@
+export default function formatDate(timestamp) {
+  const date = new Date(timestamp);
+  return date.toLocaleDateString("pt-BR", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric",
+  });
+}
