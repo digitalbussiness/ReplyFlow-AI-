f8d7e5f4c1249c9b706e0f3dd57271311eddd84d
digitalbussiness <digitalbussiness331@gmail.com>
2025-05-22 04:05:15 -0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,17 +1,17 @@
-// Importa a função para inicializar o app Firebase
 import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
-// Configuração do seu projeto Firebase (substitua pelos seus dados reais)
 const firebaseConfig = {
   apiKey: "AIzaSyAuqLUzuszv3XDu6_eyEs0ZrCfBOGQgHrc",
   authDomain: "replyflow-ai.firebaseapp.com",
   projectId: "replyflow-ai",
   storageBucket: "replyflow-ai.firebasestorage.app",
   messagingSenderId: "2282640553",
-  appId: "1:2282640553:web:1412df559aab8cb81c85b8"
+  appId: "1:2282640553:web:1412df559aab8cb81c85b8",
 };
 
-// Inicializa o app Firebase com a configuração
 const app = initializeApp(firebaseConfig);
 
-export default app;
+export const auth = getAuth(app);
+export const db = getFirestore(app);
