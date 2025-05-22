d75d11145294fc7c35f520224c8a423e88b917ec
digitalbussiness <digitalbussiness331@gmail.com>
2025-05-22 03:58:59 -0300

    Create responseTemplates.js

---
+++ src/utils/responseTemplates.js
@@ -0,0 +1,24 @@
+const responseTemplates = [
+  {
+    id: 1,
+    title: "Resposta Padrão",
+    message: "Olá! Agradeço seu contato. Em breve, responderei com mais detalhes.",
+  },
+  {
+    id: 2,
+    title: "Agradecimento",
+    message: "Muito obrigado pela sua mensagem! Fico feliz em ajudar.",
+  },
+  {
+    id: 3,
+    title: "Solicitação de Tempo",
+    message: "Oi! No momento estou ocupado, mas já te retorno assim que possível.",
+  },
+  {
+    id: 4,
+    title: "Mensagem de Encerramento",
+    message: "Agradeço a conversa. Se tiver mais dúvidas, é só chamar!",
+  },
+];
+
+export default responseTemplates;
